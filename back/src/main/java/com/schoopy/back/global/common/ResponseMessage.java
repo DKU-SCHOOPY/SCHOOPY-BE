809bbf0f116ae259e9227f0d9c69f82276feaf74
Lee Jae Hyung <nomagold57@gmail.com>
2025-02-16 15:43:21 +0900

    create global response message

---
+++ back/src/main/java/com/schoopy/back/global/common/ResponseMessage.java
@@ -0,0 +1,7 @@
+package com.schoopy.back.global.common;
+
+public interface ResponseMessage {
+    String SUCCESS = "success";
+    String DATABASE_ERROR = "database error";
+    String VALIDATION_FAIL = "validation failed";
+}
