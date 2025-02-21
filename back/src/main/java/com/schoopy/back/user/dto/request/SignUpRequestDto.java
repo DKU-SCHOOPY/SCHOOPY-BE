6dee9e8ddf8ddd6bc577e40228024159c092e70c
Lee Jae Hyung <nomagold57@gmail.com>
2025-02-21 23:58:34 +0900

    sign up request dto

---
+++ back/src/main/java/com/schoopy/back/user/dto/request/SignUpRequestDto.java
@@ -0,0 +1,18 @@
+package com.schoopy.back.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public class SignUpRequestDto {
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{12,18}$")
+    private String password;
+
+    @NotBlank
+    private String certificationNumber;
+}
