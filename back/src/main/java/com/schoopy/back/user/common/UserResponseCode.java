0c90eaa5d86861eec3f250b022cabf45f54b422c
Lee Jae Hyung <nomagold57@gmail.com>
2025-02-21 23:59:09 +0900

    user response code

---
+++ back/src/main/java/com/schoopy/back/user/common/UserResponseCode.java
@@ -0,0 +1,15 @@
+package com.schoopy.back.user.common;
+
+import com.schoopy.back.global.common.ResponseCode;
+
+public interface UserResponseCode extends ResponseCode{
+    String MAIL_FAIL = "MF";
+    String USER_NOT_EXISTS = "UNE";
+    String ENTITY_NOT_FOND = "ENF";
+    String CERTIFICATION_FAIL = "CF";
+    String DUPLICATE_EMAIL = "DE";
+    String DUPLICATE_ID = "DI";
+    String DUPLICATE_NICKNAME = "DN";
+    String EMAIL_MISSMATCH = "EM";
+    String PASSWORD_MISSMATCH = "PM";
+}
