718c99f29303b6047b239064056f7781ab831915
Lee Jae Hyung <nomagold57@gmail.com>
2025-02-23 15:47:50 +0900

    sign up request dto

--- back/src/main/java/com/schoopy/back/user/dto/request/SignUpRequestDto.java
+++ back/src/main/java/com/schoopy/back/user/dto/request/SignUpRequestDto.java
@@ -3,7 +3,13 @@ package com.schoopy.back.user.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class SignUpRequestDto {
     @Email
     @NotBlank
