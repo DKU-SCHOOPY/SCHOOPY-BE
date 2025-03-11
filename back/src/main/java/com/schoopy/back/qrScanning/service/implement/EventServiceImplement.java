8524b50604e3a716d3c57ff4197de109030482c8
Lee Jae Hyung <nomagold57@gmail.com>
2025-03-11 20:53:50 +0900

    event service implement

--- back/src/main/java/com/schoopy/back/qrScanning/service/implement/EventServiceImplement.java
+++ back/src/main/java/com/schoopy/back/qrScanning/service/implement/EventServiceImplement.java
@@ -3,6 +3,7 @@ package com.schoopy.back.qrScanning.service.implement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.schoopy.back.global.dto.ResponseDto;
 import com.schoopy.back.qrScanning.dto.request.RegistEventRequestDto;
 import com.schoopy.back.qrScanning.dto.response.RegistEventResponseDto;
 import com.schoopy.back.qrScanning.entity.EventEntity;
@@ -18,12 +19,16 @@ public class EventServiceImplement implements EventService{
     private final EventRepository eventRepository;
 
     @Override
-    public ResponseEntity<? super RegistEventResponseDto> saveEvent(RegistEventRequestDto dto) {
-        EventEntity event = EventEntity.builder()
-            .eventCode(dto.getEventCode())
-            .qrURL(dto.getQrURL())                
-            .build();
-        eventRepository.save(event);
+    public ResponseEntity<? super RegistEventResponseDto> registEvent(RegistEventRequestDto dto) {
+        try {
+            EventEntity eventEntity = new EventEntity();
+            eventEntity.setEventName(dto.getEventName());
+            eventEntity.setQrURL(dto.getQrURL());
+            eventRepository.save(eventEntity);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseDto.databaseError();
+        }
         
         return RegistEventResponseDto.success();
     }
