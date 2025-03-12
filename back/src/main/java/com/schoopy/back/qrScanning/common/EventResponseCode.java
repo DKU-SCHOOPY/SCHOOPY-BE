0ff91a98d845d0f455440371a1b20edf036297f6
Lee Jae Hyung <nomagold57@gmail.com>
2025-03-12 22:19:59 +0900

    EventResponseCode

---
+++ back/src/main/java/com/schoopy/back/qrScanning/common/EventResponseCode.java
@@ -0,0 +1,7 @@
+package com.schoopy.back.qrScanning.common;
+
+import com.schoopy.back.global.common.ResponseCode;
+
+public interface EventResponseCode extends ResponseCode{
+    String REMIT_FAIL = "RF";
+}
