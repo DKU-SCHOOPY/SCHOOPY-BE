5ff45af16fd0bb824688494c634eb9354e811f2a
Lee Jae Hyung <nomagold57@gmail.com>
2025-03-12 22:22:06 +0900

    event service

--- back/src/main/java/com/schoopy/back/qrScanning/service/EventService.java
+++ back/src/main/java/com/schoopy/back/qrScanning/service/EventService.java
@@ -3,7 +3,10 @@ package com.schoopy.back.qrScanning.service;
 import org.springframework.http.ResponseEntity;
 
 import com.schoopy.back.qrScanning.dto.request.RegistEventRequestDto;
+import com.schoopy.back.qrScanning.dto.request.RemitRequestDto;
 import com.schoopy.back.qrScanning.dto.response.RegistEventResponseDto;
+import com.schoopy.back.qrScanning.dto.response.RemitResponseDto;
 public interface EventService {
     ResponseEntity<? super RegistEventResponseDto> registEvent(RegistEventRequestDto dto);
+    ResponseEntity<? super RemitResponseDto> remitEvent(RemitRequestDto dto);
 }
