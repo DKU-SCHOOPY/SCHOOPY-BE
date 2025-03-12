3f7df55eea9f1db3425f7323aa6a272622d69079
Lee Jae Hyung <nomagold57@gmail.com>
2025-03-12 22:20:17 +0900

    EventResponseMessage

---
+++ back/src/main/java/com/schoopy/back/qrScanning/common/EventResponseMessage.java
@@ -0,0 +1,8 @@
+package com.schoopy.back.qrScanning.common;
+
+import com.schoopy.back.global.common.ResponseMessage;
+
+public interface EventResponseMessage extends ResponseMessage{
+    String REMIT_FAIL = "remit failed";
+
+}
