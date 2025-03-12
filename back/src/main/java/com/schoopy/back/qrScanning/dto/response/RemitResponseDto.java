a5db4de7f8f19ed1badfb6224d9bec598bc042b5
Lee Jae Hyung <nomagold57@gmail.com>
2025-03-12 22:20:39 +0900

    remit response dto

--- back/src/main/java/com/schoopy/back/qrScanning/dto/response/RemitResponseDto.java
+++ back/src/main/java/com/schoopy/back/qrScanning/dto/response/RemitResponseDto.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.schoopy.back.global.dto.ResponseDto;
+import com.schoopy.back.qrScanning.common.EventResponseCode;
+import com.schoopy.back.qrScanning.common.EventResponseMessage;
 
 public class RemitResponseDto extends ResponseDto{
     private RemitResponseDto () {
@@ -13,5 +15,10 @@ public class RemitResponseDto extends ResponseDto{
     public static ResponseEntity<RemitResponseDto> success() {
         RemitResponseDto responseBody = new RemitResponseDto();
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> remitFail() {
+        ResponseDto responseBody = new ResponseDto(EventResponseCode.REMIT_FAIL, EventResponseMessage.REMIT_FAIL);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
     }
 }
