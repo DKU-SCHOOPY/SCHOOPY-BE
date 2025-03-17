2cc4bfa413eb16e039e102d9936638221a2185ca
Lee Jae Hyung <nomagold57@gmail.com>
2025-03-17 22:20:26 +0900

    event repository

--- back/src/main/java/com/schoopy/back/qrScanning/repository/EventRepository.java
+++ back/src/main/java/com/schoopy/back/qrScanning/repository/EventRepository.java
@@ -3,11 +3,12 @@ package com.schoopy.back.qrScanning.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNull;
 
 import com.schoopy.back.qrScanning.entity.EventEntity;
 
+import jakarta.validation.constraints.NotBlank;
+
 public interface EventRepository extends JpaRepository<EventEntity, Long>{
 
-    Optional<EventEntity> findByEventCode(@NonNull String eventCode);
+    Optional<EventEntity> findByEventCode(@NotBlank long eventCode);
 }
