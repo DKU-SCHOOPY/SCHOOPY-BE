1c75f7d0596fa66bbc700c11ccdc13246104407a
Lee Jae Hyung <nomagold57@gmail.com>
2025-03-17 22:20:43 +0900

    certification entity

--- back/src/main/java/com/schoopy/back/user/entity/CertificationEntity.java
+++ back/src/main/java/com/schoopy/back/user/entity/CertificationEntity.java
@@ -1,19 +1,26 @@
 package com.schoopy.back.user.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@Entity(name="certification")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 @Table(name="certification")
 public class CertificationEntity {
     @Id
+    @Column(nullable = false, updatable = false)
     private String email;
+
+    @Column(nullable = false)
     private String certificationNumber;
 }
