baa31000854c8e5393c87018be5f1f8b7cb1b173
Lee Jae Hyung <nomagold57@gmail.com>
2025-03-23 20:53:09 +0900

    check certification request dto

--- back/src/main/java/com/schoopy/back/user/dto/request/CheckCertificationRequestDto.java
+++ back/src/main/java/com/schoopy/back/user/dto/request/CheckCertificationRequestDto.java
@@ -2,6 +2,7 @@ package com.schoopy.back.user.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,9 +11,9 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class CheckCertificationRequestDto {
-    @Email
     @NotBlank
-    private String email;
+    @Pattern(regexp = "\\d{8}", message = "학번은 8자리 숫자여야 합니다.")
+    private String studentNum;
 
     @NotBlank
     private String certificationNumber;
