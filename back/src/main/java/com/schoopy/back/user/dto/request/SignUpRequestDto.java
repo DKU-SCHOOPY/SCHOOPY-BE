27482dc3135b8fed87dd6c19c06946154e841a77
Lee Jae Hyung <nomagold57@gmail.com>
2025-03-23 20:53:57 +0900

    sign up request dto

--- back/src/main/java/com/schoopy/back/user/dto/request/SignUpRequestDto.java
+++ back/src/main/java/com/schoopy/back/user/dto/request/SignUpRequestDto.java
@@ -11,14 +11,29 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class SignUpRequestDto {
-    @Email
     @NotBlank
-    private String email;
+    @Pattern(regexp = "\\d{8}", message = "학번은 8자리 숫자여야 합니다.")
+    private String studentNum;
 
     @NotBlank
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{12,18}$")
     private String password;
 
     @NotBlank
+    private String name;
+
+    @NotBlank
     private String certificationNumber;
+
+    @NotBlank
+    private String department;
+
+    @NotBlank
+    private String gender;
+
+    @NotBlank
+    private String birthDay;
+
+    @NotBlank 
+    private String phoneNum;
 }
