ab340f63baa3c56073d948de9e3290f0a5f8d075
Lee Jae Hyung <nomagold57@gmail.com>
2025-04-21 16:32:30 +0900

    submit survey repository

--- back/src/main/java/com/schoopy/back/event/repository/SubmitSurveyRepository.java
+++ back/src/main/java/com/schoopy/back/event/repository/SubmitSurveyRepository.java
@@ -2,7 +2,6 @@ package com.schoopy.back.event.repository;
 
 import com.schoopy.back.event.entity.EventEntity;
 import com.schoopy.back.event.entity.SubmitSurveyEntity;
-import com.schoopy.back.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
