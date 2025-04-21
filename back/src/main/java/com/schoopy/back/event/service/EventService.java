b0c3e852f6c8dbb9967b478ecedac6e2d3d7adab
Lee Jae Hyung <nomagold57@gmail.com>
2025-04-21 16:32:36 +0900

    event service

--- back/src/main/java/com/schoopy/back/event/service/EventService.java
+++ back/src/main/java/com/schoopy/back/event/service/EventService.java
@@ -5,6 +5,7 @@ import com.schoopy.back.event.dto.request.SubmitSurveyRequestDto;
 import com.schoopy.back.event.dto.request.UpdatePaymentStatusRequestDto;
 import com.schoopy.back.event.dto.response.SubmitSurveyResponseDto;
 import com.schoopy.back.event.dto.response.UpdatePaymentStatusResponseDto;
+import com.schoopy.back.event.dto.response.CalendarResponseDto;
 import com.schoopy.back.event.entity.EventEntity;
 import com.schoopy.back.event.entity.SubmitSurveyEntity;
 import org.springframework.http.ResponseEntity;
@@ -21,4 +22,5 @@ public interface EventService {
     List<EventEntity> getCurrentSurveyEvents();
     List<SubmitSurveyEntity> getSubmissionsByEvent(Long eventCode);
     ResponseEntity<? super UpdatePaymentStatusResponseDto> updatePaymentStatus(UpdatePaymentStatusRequestDto dto);
+    List<CalendarResponseDto> getCalendarEventsByYearAndMonth(int year, int month);
 }
