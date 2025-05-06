4ef8f7bbbcf39e4a7ae930622566f65f8928fd66
Lee Jae Hyung <nomagold57@gmail.com>
2025-05-06 15:32:42 +0900

    fcm response message

---
+++ back/src/main/java/com/schoopy/back/fcm/comomn/FcmResponseMessage.java
@@ -0,0 +1,7 @@
+package com.schoopy.back.fcm.comomn;
+
+import com.schoopy.back.global.common.ResponseMessage;
+
+public interface FcmResponseMessage extends ResponseMessage{    
+    String SEND_FAIL = "notification send failed";
+}
