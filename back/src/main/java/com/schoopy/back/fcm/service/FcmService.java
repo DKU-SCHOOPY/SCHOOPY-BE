126a9aabd275599ee997dfffeff70c3739e557d7
Lee Jae Hyung <nomagold57@gmail.com>
2025-05-06 15:32:56 +0900

    fcm service

--- back/src/main/java/com/schoopy/back/fcm/service/FcmService.java
+++ back/src/main/java/com/schoopy/back/fcm/service/FcmService.java
@@ -1,8 +1,13 @@
 package com.schoopy.back.fcm.service;
 
-import com.schoopy.back.fcm.dto.FcmMessageDto;
+import org.springframework.http.ResponseEntity;
+import com.schoopy.back.fcm.dto.response.DepartmentFcmResponseDto;
+
+import com.schoopy.back.fcm.dto.request.DepartmentFcmRequestDto;
+import com.schoopy.back.fcm.dto.request.FcmMessageDto;
 
 public interface FcmService {
     void sendMessageTo(FcmMessageDto dto);
     void sendMessageByTopic(String title, String body);
+    ResponseEntity<? super DepartmentFcmResponseDto> sendMessageToDepartment(DepartmentFcmRequestDto dto);
 }
