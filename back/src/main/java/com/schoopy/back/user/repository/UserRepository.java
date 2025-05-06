ccb0d15158d395c6ee2df72e74bf8bdf653ca8a9
Lee Jae Hyung <nomagold57@gmail.com>
2025-05-06 15:33:11 +0900

    user repository

--- back/src/main/java/com/schoopy/back/user/repository/UserRepository.java
+++ back/src/main/java/com/schoopy/back/user/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.schoopy.back.user.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.schoopy.back.user.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, String>{
     boolean existsByStudentNum(String studentNum);
-
     UserEntity findByStudentNum(String studentNum);
+    List<UserEntity> findAllByDepartment(String department);
 }
