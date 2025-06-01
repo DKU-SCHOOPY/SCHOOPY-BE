f1b435e9b1f5d3f773d2c051c72622f2ad8f6407
Lee Jae Hyung <nomagold57@gmail.com>
2025-06-01 18:02:51 +0900

    notice entity

---
+++ back/src/main/java/com/schoopy/back/notice/entity/NoticeEntity.java
@@ -0,0 +1,40 @@
+package com.schoopy.back.notice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="notice")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="notice")
+public class NoticeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private long noticeId;
+
+    @Column(nullable = false)
+    private String sender;
+
+    @Column(nullable = false)
+    private String reciever;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private boolean check;
+}
