42cfa3e151c4ed07c6029c3d428efd0befc8c0ac
Lee Jae Hyung <nomagold57@gmail.com>
2025-06-01 18:02:59 +0900

    notice repository

---
+++ back/src/main/java/com/schoopy/back/notice/repository/NoticeRepository.java
@@ -0,0 +1,11 @@
+package com.schoopy.back.notice.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.schoopy.back.notice.entity.NoticeEntity;
+
+public interface NoticeRepository extends JpaRepository<NoticeEntity, Long>{
+    List<NoticeEntity> findByReciever(String reciever);
+}
