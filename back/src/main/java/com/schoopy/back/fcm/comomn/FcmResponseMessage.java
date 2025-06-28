bedf4170082d1108677e3399c15080944064a708
now-brother <nomagold57@gmail.com>
2025-06-28 15:20:38 +0900

    Delete fcm function

--- back/src/main/java/com/schoopy/back/fcm/comomn/FcmResponseMessage.java
+++
@@ -1,7 +0,0 @@
-package com.schoopy.back.fcm.comomn;
-
-import com.schoopy.back.global.common.ResponseMessage;
-
-public interface FcmResponseMessage extends ResponseMessage{    
-    String SEND_FAIL = "notification send failed";
-}
