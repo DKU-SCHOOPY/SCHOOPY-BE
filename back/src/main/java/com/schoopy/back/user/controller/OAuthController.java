8d4553c448bd16d2e55ef28bdd3255fb4671d34f
now-brother <nomagold57@gmail.com>
2025-07-07 09:04:37 +0900

    oauth controller

--- back/src/main/java/com/schoopy/back/user/controller/OAuthController.java
+++ back/src/main/java/com/schoopy/back/user/controller/OAuthController.java
@@ -2,13 +2,20 @@ package com.schoopy.back.user.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.schoopy.back.user.dto.request.LinkKakaoRequestDto;
+import com.schoopy.back.user.dto.request.LinkNaverRequestDto;
+import com.schoopy.back.user.dto.response.ChangePhoeNumResponseDto;
+import com.schoopy.back.user.dto.response.LinkSocialResponseDto;
 import com.schoopy.back.user.dto.response.SignInResponseDto;
 import com.schoopy.back.user.service.UserService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -30,5 +37,21 @@ public class OAuthController {
         @RequestParam("code") String code
     ) {
         return userService.kakaoLogin(code);
+    }
+
+    @PostMapping("/kakao/link")
+    public ResponseEntity<? super LinkSocialResponseDto> kakaoLink(
+        @RequestBody @Valid LinkKakaoRequestDto requestBody
+    ) {
+        ResponseEntity<? super LinkSocialResponseDto> response = userService.kakaoLink(requestBody);
+        return response;
+    }
+
+    @PostMapping("/naver/link")
+    public ResponseEntity<? super LinkSocialResponseDto> naverLink(
+        @RequestBody @Valid LinkNaverRequestDto requestBody
+    ) {
+        ResponseEntity<? super LinkSocialResponseDto> response = userService.naverLink(requestBody);
+        return response;
     }
 }
