3ca6d276d4b8ec3aee91b12a4c252ef2693d05ab
now-brother <nomagold57@gmail.com>
2025-07-07 09:04:56 +0900

    link kakao request dto

---
+++ back/src/main/java/com/schoopy/back/user/dto/request/LinkKakaoRequestDto.java
@@ -0,0 +1,18 @@
+package com.schoopy.back.user.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LinkKakaoRequestDto {
+    
+    @NotBlank
+    private String studentNum;
+
+    @NotBlank
+    private String code;
+}
