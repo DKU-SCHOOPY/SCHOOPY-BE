6ffd19743e82ef414ebe6ce6afb6e54ddacd717a
now-brother <nomagold57@gmail.com>
2025-07-07 09:05:10 +0900

    user service

--- back/src/main/java/com/schoopy/back/user/service/UserService.java
+++ back/src/main/java/com/schoopy/back/user/service/UserService.java
@@ -17,4 +17,6 @@ public interface UserService {
     ResponseEntity<? super ChangePhoeNumResponseDto> changePhoneNum (ChangePhoneNumRequestDto dto);
     ResponseEntity<? super SignInResponseDto> naverLogin(String code, String state);
     ResponseEntity<? super SignInResponseDto> kakaoLogin(String code);
+    ResponseEntity<? super LinkSocialResponseDto> kakaoLink(LinkKakaoRequestDto dto);
+    ResponseEntity<? super LinkSocialResponseDto> naverLink(LinkNaverRequestDto dto);
 }
