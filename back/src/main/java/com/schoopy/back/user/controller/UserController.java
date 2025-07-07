b09a7087de86071729ef7e20fcb9a6556a3b2399
now-brother <nomagold57@gmail.com>
2025-07-07 09:04:46 +0900

    user controller

--- back/src/main/java/com/schoopy/back/user/controller/UserController.java
+++ back/src/main/java/com/schoopy/back/user/controller/UserController.java
@@ -1,11 +1,8 @@
 package com.schoopy.back.user.controller;
 
-import java.util.Map;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
