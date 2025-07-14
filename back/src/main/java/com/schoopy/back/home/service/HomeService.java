a38e879ca2cd0a142228650843e090973fc1d69f
now-brother <nomagold57@gmail.com>
2025-07-15 00:37:30 +0900

    home service : add get event information fuction

--- back/src/main/java/com/schoopy/back/home/service/HomeService.java
+++ back/src/main/java/com/schoopy/back/home/service/HomeService.java
@@ -2,9 +2,14 @@ package com.schoopy.back.home.service;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import com.schoopy.back.event.entity.EventEntity;
+import com.schoopy.back.home.dto.request.GetEventInformationRequestDto;
+import com.schoopy.back.home.dto.response.*;
 
 public interface HomeService {
         List<EventEntity> getAllEvents();
+        ResponseEntity<? super GetEventInformationResponseDto> getEventInformation(GetEventInformationRequestDto requestBody);
 
 }
