4286553edffbfa8f0f6d07783ca7e19f9e7c5066
now-brother <nomagold57@gmail.com>
2025-07-15 00:36:55 +0900

    home controller : add get event information fuction

--- back/src/main/java/com/schoopy/back/home/controller/HomeController.java
+++ back/src/main/java/com/schoopy/back/home/controller/HomeController.java
@@ -8,9 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.schoopy.back.event.entity.EventEntity;
+import com.schoopy.back.home.dto.request.GetEventInformationRequestDto;
+import com.schoopy.back.home.dto.response.GetEventInformationResponseDto;
 import com.schoopy.back.home.service.HomeService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -23,4 +29,13 @@ public class HomeController {
     public ResponseEntity<List<EventEntity>> getAllEvents() {
         return ResponseEntity.ok(homeService.getAllEvents());
     }
+
+    @PostMapping("/getEvent")
+    public ResponseEntity<? super GetEventInformationResponseDto> getEventInformation(
+        @RequestBody @Valid GetEventInformationRequestDto requestBody
+    ) {
+        ResponseEntity<? super GetEventInformationResponseDto> response = homeService.getEventInformation(requestBody);
+        return response;
+    }
+    
 }
