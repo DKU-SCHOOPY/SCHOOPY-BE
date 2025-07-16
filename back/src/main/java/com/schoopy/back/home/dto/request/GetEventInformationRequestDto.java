4b619bc50da21003452d1b2c0b0774de5eaefb29
now-brother <nomagold57@gmail.com>
2025-07-16 14:55:36 +0900

    get event information request dto

--- back/src/main/java/com/schoopy/back/home/dto/request/GetEventInformationRequestDto.java
+++ back/src/main/java/com/schoopy/back/home/dto/request/GetEventInformationRequestDto.java
@@ -1,6 +1,6 @@
 package com.schoopy.back.home.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +10,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class GetEventInformationRequestDto {
 
-    @NotBlank
+    @NotNull
     private Long eventCode;
 }
