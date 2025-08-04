1453402c17f1a91ca87a7b616c060aa593e28e4e
now-brother <nomagold57@gmail.com>
2025-08-04 16:50:17 +0900

    jwt provider

--- back/src/main/java/com/schoopy/back/global/provider/JwtProvider.java
+++ back/src/main/java/com/schoopy/back/global/provider/JwtProvider.java
@@ -18,14 +18,16 @@ public class JwtProvider {
     @Value("${secret-key}")
     private String secretKey;
 
-    public String create(String studentNum) {
+    public String create(String studentNum, String role) {
 
         Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
         Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
 
         String jwt = Jwts.builder()
             .signWith(key, SignatureAlgorithm.HS256)
-            .setSubject(studentNum).setIssuedAt(new Date()).setExpiration(expiredDate)
+            .setSubject(studentNum)
+            .setIssuedAt(new Date())
+            .setExpiration(expiredDate)
             .compact();
 
         return jwt;
