3aefc6f44edc78ff215fcbe5d211885a20bc3d22
now-brother <nomagold57@gmail.com>
2025-08-14 10:49:45 +0900

    get event information response dto

--- back/src/main/java/com/schoopy/back/home/dto/response/GetEventInformationResponseDto.java
+++ back/src/main/java/com/schoopy/back/home/dto/response/GetEventInformationResponseDto.java
@@ -1,11 +1,13 @@
 package com.schoopy.back.home.dto.response;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.schoopy.back.event.entity.EventEntity;
+import com.schoopy.back.event.entity.FormEntity;
 import com.schoopy.back.global.dto.ResponseDto;
 
 import lombok.Getter;
@@ -18,19 +20,29 @@ public class GetEventInformationResponseDto extends ResponseDto {
     private String department;
     private String eventDescription;
     private List<String> eventImages;
+    private LocalDate surveyStartDate;
+    private LocalDate surveyEndDate;
+    private LocalDate eventStartDate;
+    private LocalDate eventEndDate;
+    private int maxParticipant;
 
     
-    private GetEventInformationResponseDto(EventEntity event) {
+    private GetEventInformationResponseDto(EventEntity event, FormEntity form) {
         super();
         this.eventCode = event.getEventCode();
         this.eventName = event.getEventName();
         this.department = event.getDepartment();
         this.eventDescription = event.getEventDescription();
         this.eventImages = event.getEventImages();
+        this.surveyStartDate = form.getSurveyStartDate();
+        this.surveyEndDate = form.getSurveyEndDate();
+        this.eventStartDate = event.getEventStartDate();
+        this.eventEndDate = event.getEventEndDate();
+        this.maxParticipant = form.getMaxParticipants();
     }
 
-    public static ResponseEntity<GetEventInformationResponseDto> success(EventEntity event) {
-        GetEventInformationResponseDto responseBody = new GetEventInformationResponseDto(event);
+    public static ResponseEntity<GetEventInformationResponseDto> success(EventEntity event, FormEntity form) {
+        GetEventInformationResponseDto responseBody = new GetEventInformationResponseDto(event, form);
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     } 
 
