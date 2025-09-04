d8bcd8924c859159e8ba062e3480961ccf7c17dd
now-brother <nomagold57@gmail.com>
2025-09-04 19:57:51 +0900

    notice entity

--- back/src/main/java/com/schoopy/back/notice/entity/NoticeEntity.java
+++ back/src/main/java/com/schoopy/back/notice/entity/NoticeEntity.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,11 +27,13 @@ public class NoticeEntity {
     @Column(nullable = false)
     private long noticeId;
 
-    @Column(nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false) 
     private UserEntity sender;
 
-    @Column(nullable = false)
-    private UserEntity reciever;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private UserEntity receiver;
 
     @Column(nullable = false)
     private String title;
@@ -45,11 +49,12 @@ public class NoticeEntity {
 
 
 
-    public NoticeEntity(UserEntity sender, UserEntity reciever, String title, String message, boolean isPresident) {
+    public NoticeEntity(UserEntity sender, UserEntity receiver, String title, String message, boolean isPresident) {
         this.sender = sender;
-        this.reciever = reciever;
+        this.receiver = receiver;
         this.title = title;
         this.message = message;
         this.readCheck = false;
+        this.isPresident = isPresident;
     }
 }
