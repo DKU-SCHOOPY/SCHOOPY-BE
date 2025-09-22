eca786d3668d57666a585d04b2f8b9a4b22cfc22
now-brother <nomagold57@gmail.com>
2025-09-22 11:26:36 +0900

    get event information response dto

--- back/src/main/java/com/schoopy/back/home/dto/response/GetEventInformationResponseDto.java
+++ back/src/main/java/com/schoopy/back/home/dto/response/GetEventInformationResponseDto.java
@@ -41,9 +41,28 @@ public class GetEventInformationResponseDto extends ResponseDto {
         this.maxParticipant = form.getMaxParticipants();
     }
 
+    private GetEventInformationResponseDto(EventEntity event) {
+        super();
+        this.eventCode = event.getEventCode();
+        this.eventName = event.getEventName();
+        this.department = event.getDepartment();
+        this.eventDescription = event.getEventDescription();
+        this.eventImages = event.getEventImages();
+        this.surveyStartDate = null;
+        this.surveyEndDate = null;
+        this.eventStartDate = event.getEventStartDate();
+        this.eventEndDate = event.getEventEndDate();
+        this.maxParticipant = 0;
+    }
+
     public static ResponseEntity<GetEventInformationResponseDto> success(EventEntity event, FormEntity form) {
         GetEventInformationResponseDto responseBody = new GetEventInformationResponseDto(event, form);
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     } 
+
+    public static ResponseEntity<GetEventInformationResponseDto> success(EventEntity event) {
+        GetEventInformationResponseDto responseBody = new GetEventInformationResponseDto(event);
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
 
 }
