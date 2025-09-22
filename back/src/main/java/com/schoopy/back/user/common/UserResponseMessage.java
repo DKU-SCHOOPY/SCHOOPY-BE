803e74b40f4402a236c2f8059028c39157b539f0
now-brother <nomagold57@gmail.com>
2025-09-22 09:53:03 +0900

    user response message

--- back/src/main/java/com/schoopy/back/user/common/UserResponseMessage.java
+++ back/src/main/java/com/schoopy/back/user/common/UserResponseMessage.java
@@ -7,7 +7,7 @@ public interface UserResponseMessage extends ResponseMessage{
     String USER_NOT_EXISTS = "user is not exists";
     String DUPLICATE_EMAIL = "duplicated email";
     String DUPLICATE_ID = "duplicate id";
-    String ENTITY_NOT_FOND = "entity not found";
+    String ENTITY_NOT_FOUND = "entity not found";
     String CERTIFICATION_FAIL = "certification failed";
     String DUPLICATE_NICKNAME = "duplicated nickname";
     String EMAIL_MISSMATCH = "email missmatch";
