2c7e37676217fb3fe9fed6912818ca684b71d79d
now-brother <nomagold57@gmail.com>
2025-10-13 11:34:59 +0900

    mypage service

--- back/src/main/java/com/schoopy/back/mypage/service/MypageService.java
+++ back/src/main/java/com/schoopy/back/mypage/service/MypageService.java
@@ -4,12 +4,15 @@ import org.springframework.http.ResponseEntity;
 import com.schoopy.back.mypage.dto.request.MypageRequestDto;
 import com.schoopy.back.mypage.dto.request.ChangeDeptRequestDto;
 import com.schoopy.back.mypage.dto.request.ChangePhoneNumRequestDto;
+import com.schoopy.back.mypage.dto.request.CouncilMypageRequestDto;
 import com.schoopy.back.mypage.dto.response.MypageResponseDto;  
 import com.schoopy.back.mypage.dto.response.ChangeDeptResponseDto;
 import com.schoopy.back.mypage.dto.response.ChangePhoeNumResponseDto;
+import com.schoopy.back.mypage.dto.response.CouncilMypageResponseDto;
 
 public interface MypageService {
     ResponseEntity<? super MypageResponseDto> printMypage (MypageRequestDto dto);
     ResponseEntity<? super ChangeDeptResponseDto> changeDept (ChangeDeptRequestDto dto);
     ResponseEntity<? super ChangePhoeNumResponseDto> changePhoneNum (ChangePhoneNumRequestDto dto);
+    ResponseEntity<? super CouncilMypageResponseDto> printCouncilMypage (CouncilMypageRequestDto dto);
 }
