11d24020b693edd74d7865e7aa360b047f9c6aa9
now-brother <nomagold57@gmail.com>
2025-10-13 13:15:41 +0900

    change enrollment response dto

---
+++ back/src/main/java/com/schoopy/back/mypage/dto/response/ChangeEnrollResponseDto.java
@@ -0,0 +1,18 @@
+package com.schoopy.back.mypage.dto.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.schoopy.back.global.dto.ResponseDto;
+
+public class ChangeEnrollResponseDto extends ResponseDto{
+    private ChangeEnrollResponseDto () {
+        super();
+    }
+
+    public static ResponseEntity<ChangeEnrollResponseDto> success() {
+        ChangeEnrollResponseDto responseBody = new ChangeEnrollResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+    
+}
