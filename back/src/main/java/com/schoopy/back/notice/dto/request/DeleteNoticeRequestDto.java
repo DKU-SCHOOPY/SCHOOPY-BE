1f2090c3f35cca9d9ab41ba6bc88cc49fba3a93a
now-brother <nomagold57@gmail.com>
2025-10-20 15:30:48 +0900

    delete notice request dto

--- back/src/main/java/com/schoopy/back/notice/dto/request/DeleteNoticeRequestDto.java
+++ back/src/main/java/com/schoopy/back/notice/dto/request/DeleteNoticeRequestDto.java
@@ -1,8 +1,12 @@
 package com.schoopy.back.notice.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class DeleteNoticeRequestDto {
     private Long noticeId;    
 }
