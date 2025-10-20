a79b77baf65364af02f99b4ac7b2c9658728850b
now-brother <nomagold57@gmail.com>
2025-10-20 15:38:25 +0900

    home service implement

--- back/src/main/java/com/schoopy/back/home/service/implement/HomeServiceImplement.java
+++ back/src/main/java/com/schoopy/back/home/service/implement/HomeServiceImplement.java
@@ -42,15 +42,15 @@ public class HomeServiceImplement implements HomeService{
         int pnoticeCount = 0;
 
         //안읽은 알림 개수 설정
-            if(presidentRepository.existsById(dto.getStudentNum())) {
-                pnoticeCount = noticeRepository.countByReceiverAndReadCheckAndIsPresident(userEntity, false, true);
-                noticeCount = noticeRepository.countByReceiverAndReadCheckAndIsPresident(userEntity, false, false);
-                userEntity.setPNoticeCount(pnoticeCount);
-                userEntity.setNoticeCount(noticeCount);
-            }else {
-                noticeCount = noticeRepository.countByReceiverAndReadCheck(userEntity, false);
-                userEntity.setNoticeCount(noticeCount);
-            }
+        if(presidentRepository.existsById(dto.getStudentNum())) {
+            pnoticeCount = noticeRepository.countByReceiverAndReadCheckAndIsPresident(userEntity, false, true);
+            noticeCount = noticeRepository.countByReceiverAndReadCheckAndIsPresident(userEntity, false, false);
+            userEntity.setPNoticeCount(pnoticeCount);
+            userEntity.setNoticeCount(noticeCount);
+        }else {
+            noticeCount = noticeRepository.countByReceiverAndReadCheck(userEntity, false);
+            userEntity.setNoticeCount(noticeCount);
+        }
 
         int nc;
 
