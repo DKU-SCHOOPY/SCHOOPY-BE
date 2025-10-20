b18ffa5def1e99e8215c53ff13d16c1a3de44752
now-brother <nomagold57@gmail.com>
2025-10-20 15:29:54 +0900

    change enroll request request dto

---
+++ back/src/main/java/com/schoopy/back/mypage/dto/request/ChangeEnrollRequestRequestDto.java
@@ -0,0 +1,12 @@
+package com.schoopy.back.mypage.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class ChangeEnrollRequestRequestDto {
+    private String studentNum;
+}
