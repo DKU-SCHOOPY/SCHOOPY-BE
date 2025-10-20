66a79ba3f9eda1f81a2d8712294fa2a269b26ecd
now-brother <nomagold57@gmail.com>
2025-10-20 15:30:41 +0900

    c request dto

---
+++ back/src/main/java/com/schoopy/back/notice/dto/request/CRequestDto.java
@@ -0,0 +1,14 @@
+package com.schoopy.back.notice.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CRequestDto {
+    private long noticeId;
+    private boolean accept;
+    private boolean SW;
+}
