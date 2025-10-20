3bc19468c3725c3de6c77a53c3a5a20dac5cde50
now-brother <nomagold57@gmail.com>
2025-10-20 15:30:53 +0900

    e request dto

---
+++ back/src/main/java/com/schoopy/back/notice/dto/request/ERequestDto.java
@@ -0,0 +1,13 @@
+package com.schoopy.back.notice.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ERequestDto {
+    private boolean accept;
+    private long noticeId;
+}
