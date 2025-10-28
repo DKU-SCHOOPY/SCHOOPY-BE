942ef55774b3835f9372ca010540cdb1e0dcb89c
now-brother <nomagold57@gmail.com>
2025-10-28 10:14:51 +0900

    sign up request dto

--- back/src/main/java/com/schoopy/back/user/dto/request/SignUpRequestDto.java
+++ back/src/main/java/com/schoopy/back/user/dto/request/SignUpRequestDto.java
@@ -15,7 +15,7 @@ public class SignUpRequestDto {
     private String studentNum;
 
     @NotBlank
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{12,18}$")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{8,20}$")
     private String password;
 
     @NotBlank
